d0e6607ef30050459c43c05c9ff99b88ba143ae7
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2025-01-25 12:29:21 -0800

    implement csv reader

--- lab2/c45.py
+++ lab2/c45.py
@@ -9,16 +9,32 @@ class c45:
         self.metric = metric
         self.threshold = threshold
         self.tree = None
+        self.labels = None
 
     def fit(self, train_x, train_y):
         pass
 
-    def predict(self, X_test):
+    def predict(self, X_test_df):
         if self.tree is None:
             print("Tree is not trained, call fit() or read_tree() first")
-            return None
-        pass
 
+        self.labels = X_test_df.keys()
+
+        return [self.predict_one(self.tree["node"], x) for x in X_test_df.data]
+    
+    def predict_one(self, node, x):
+        x_value = x[self.labels.get_loc(node["var"])]
+
+        for edge_dict in node["edges"]:
+            edge = edge_dict["edge"]
+            if edge["value"] == x_value:
+                if "leaf" in edge:
+                    return edge["leaf"]
+                else:
+                    return self.predict_one(edge["node"], x)
+                
+        Exception("No edge found for value", x_value, "in node", node)
+        
     def save_tree(self, filename):
         pass
 
@@ -26,6 +42,6 @@ class c45:
         try:
             self.tree = json.load(open(filename, "r"))
             print("Tree loaded from", filename)
-        except:
-            print("Error reading tree from", filename)
+        except Exception as e:
+            print("Error reading tree from", filename, ":", e)
 
---
+++ lab2/csv_reader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import io
+
+def read_csv(filename):
+    '''
+    reads the special csv format used in this lab
+    returns a tuple with:
+    - domain (as a list of int)
+    - class_var (as a string)
+    - dataframe of values
+    '''
+    # read file as string
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    # read special info
+    domain = data.split("\n")[1].split(",")
+    class_var = data.split("\n")[2]
+
+    # remove second and third line, so it looks like a normal csv
+    data = data.split("\n", 2)[0] + data.split("\n", 3)[3]
+
+    return domain, class_var, pd.read_csv(io.StringIO(data))
+
+print(read_csv("csv/nursery.csv")[0])
