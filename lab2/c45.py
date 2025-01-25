1ff276cc1d87cb66780415e57f4882a62a4ee362
Lucas Summers <lucas.summers.dev@gmail.com>
2025-01-24 17:25:25 -0800

    start

---
+++ lab2/c45.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import json
+
+class c45:
+    def __init__(self, metric="information_gain", threshold=0.01):
+        self.metric = metric
+        self.threshold = threshold
+        self.tree = None
+
+    def fit(train_x, train_y):
+        pass
+
+    def predict(X_test):
+        pass
+
+    def save_tree(filename):
+        pass
+
+    def read_tree(filename):
+        pass
