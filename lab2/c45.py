7cc843adcd92b49e1fdaa7be613d10e53a0bd528
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2025-01-25 11:23:01 -0800

    add gitignore and reading from json

--- lab2/c45.py
+++ lab2/c45.py
@@ -3,19 +3,29 @@ import numpy as np
 import json
 
 class c45:
-    def __init__(self, metric="information_gain", threshold=0.01):
+    def __init__(self, metric="info_gain", threshold=0.01):
+        assert metric in ["info_gain", "gain_ratio"]
+
         self.metric = metric
         self.threshold = threshold
         self.tree = None
 
-    def fit(train_x, train_y):
+    def fit(self, train_x, train_y):
         pass
 
-    def predict(X_test):
+    def predict(self, X_test):
+        if self.tree is None:
+            print("Tree is not trained, call fit() or read_tree() first")
+            return None
         pass
 
-    def save_tree(filename):
+    def save_tree(self, filename):
         pass
 
-    def read_tree(filename):
-        pass
+    def read_tree(self, filename):
+        try:
+            self.tree = json.load(open(filename, "r"))
+            print("Tree loaded from", filename)
+        except:
+            print("Error reading tree from", filename)
+
